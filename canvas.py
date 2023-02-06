cf5a2ce01d0a54b833664a102e84bc3f2e827a2e
ben <bewerner23@gmail.com>
2023-02-06 18:33:36 -0500

    update

--- canvas.py
+++ canvas.py
@@ -1,8 +1,47 @@
 from decouple import config
+import requests
+
 
 API_KEY = config('API_KEY')
+API_URL = config('API_URL')
+API_TOKEN_IDENTIFIER = '?access_token=' # If using OAuth, this should be different.
 
 def main():
-    print(API_KEY)
+    response = execute('GET', '/api/v1/courses', '')
+
+    print()
+    #print(response.json())
+    # for item in response.json():
+    #     for key in item:
+    #         if (key == 'name'):
+    #             print(key, item[key])
+
+    print(response.json())
+                
+
+def test():
+        response = requests.get(API_URL + '/api/v1/courses/1424216/permissions?access_token=' + API_KEY)
+    #print(response.json())
+    # for item in response.json():
+    #     for key in item:
+    #         if (key == 'name'):
+    #             print(key, item[key])
+
+        print(response.json())
+
+def execute(type, url, data):
+    if (type == 'GET'):
+        response = requests.get(API_URL + url + API_TOKEN_IDENTIFIER + API_KEY)
+    elif (type == 'POST'):
+        response = requests.post(API_URL + url + API_TOKEN_IDENTIFIER + API_KEY, data)
+    elif (type == 'PUT'):
+        response = requests.put(API_URL + url + API_TOKEN_IDENTIFIER + API_KEY, data)
+    elif (type == 'DELETE'):
+        response = requests.delete(API_URL + url + API_TOKEN_IDENTIFIER + API_KEY)
+    else:
+        print('Invalid request type')
+        return None
+
+    return response
 
 main()
--- readme.md
+++ readme.md
@@ -2,6 +2,7 @@
 
 # Python Packages:
 pip install python-decouple
+pip install requests
 
 # Adding Your API Key
 To add your API_KEY to the script, make a new .env file in the same directory as canvas.py, then set the API_KEY variable to your API key. 
@@ -11,3 +12,13 @@ For example, if your API key is 123456789, then your .env file should look like 
 
 ```API_KEY = 123456789```
 
+# Adding The Canvas API_LINK
+To add your API_LINK to the script, in the .env file you made add the following entry:
+
+```API_URL = 'https://webcourses.ucf.edu'``` 
+
+The idea here is we can try different links.
+
+# Helpful links:
+
+Canvas Documentation: https://canvas.instructure.com/doc/api/
